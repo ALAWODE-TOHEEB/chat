0b25a54f6ab090beb195fd6667fb3d1ba10088f1
ALAWODE-TOHEEB <105168353+ALAWODE-TOHEEB@users.noreply.github.com>
2023-09-26 07:14:16 -0700

    Update index.js
    today's 1

--- index.js
+++ index.js
@@ -4,34 +4,33 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
-
-app.use(cors()); // Use the cors middleware;
+app.use(cors()); // to use the cors middleware;
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
-  },
+    cors: {
+        origin:" http://localhost:5173",
+        methods : ["GET", "POST"]
+},
 });
 
+
+
 io.on("connection", (socket) => {
-    console.log(` a user connected : ${socket.id}`);
+    console.log(` User Connected : ${socket.id}`);
 
-    socket.on("dynasty", function (re) {
-    //     var jsonMap = {
-    //         'message': re['message'], // Assuming 'text' is a field in 're'
-    //         'dynasty_id': re['dynasty_id'],     // Assuming 'id' is a field in 're'
-    //         // 'sent_at': DateTime.now().toString(), 
-    //         'user_id' :  re['user_id']         // You can customize the time format
-    //       };
-
-        io.emit("received-dynasty",{message:re} )
-        console.log(re);
+    socket.on("send-message", (message) => {
+        console.log(message);
+        io.emit("received-message", message);
     })
-})
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.id)
+    });
+});
+
 
 server.listen(5000, () => {
-  console.log("SEVER RUNNING");
+    console.log("SEVER RUNNING");
 });
